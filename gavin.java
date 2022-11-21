fc8933d4ba09ce70d5c9fe63fb2ff06e583bc80c
Anderson <andersong6@winthrop.edu>
2022-11-21 14:50:54 -0500

    Adding the base texture of gavin.java

---
+++ gavin.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class SQLDatabaseConnection {
+    //connecting to database
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://deltona.birdnest.org:3306"
+                        + "database=my_andersong6_gavin;"
+                        + "user=my.andersong6;"
+                        + "password=145d8#h%;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            // TODO: 
+        }       Statement statement = connection.createStatement();) {
+
+            // Allow user to type in information to add to the database
+           
+
+            // Print results from select statement for testing purposes
+            
+
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
