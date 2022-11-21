3d2c90b78892f519557a1bca8db9e84dc2643818
Solid Wires <solid.wires@gmail.com>
2022-11-21 15:02:54 -0500

    Fixed this

--- Jared.java
+++ Jared.java
@@ -2,7 +2,7 @@
 //  https://www.tutorialspoint.com/java_mysql/java_mysql_select_database.htm
 public class Jared {
 
-    static String databaseUrl = "deltona.birdnest.org";
+
 
     public static void query() {
         
