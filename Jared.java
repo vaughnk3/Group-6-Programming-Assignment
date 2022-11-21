f091f294b6550050e35e17887bfe15a10c57814c
Solid Wires <solid.wires@gmail.com>
2022-11-21 15:16:10 -0500

    Trying to understand how programming this query works

--- Jared.java
+++ Jared.java
@@ -3,13 +3,16 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class Jared {
 
     public static void query() {
+        String command = "";
         try { 
             Connection connection = DriverManager.getConnection(SQLDatabaseConnection.connectionUrl);
-            
+            Statement stmt = connection.createStatement();
+            stmt.executeQuery(command);
         } catch (Exception e) {
             e.printStackTrace();
         }
