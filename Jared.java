1d6a664b1bbb6c165513427d72aa589be80a41a6
Solid Wires <solid.wires@gmail.com>
2022-12-04 22:13:29 -0500

    Added my own CRUD into the project

--- Jared.java
+++ Jared.java
@@ -1,5 +1,5 @@
 //Reference (based from):
-//  https://www.tutorialspoint.com/java_mysql/java_mysql_select_database.html
+//  https://www.tutorialspoint.com/java_mysql/java_mysql_select_database
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,6 +12,7 @@ public class Jared {
         try { 
             Connection connection = DriverManager.getConnection(SQLDatabaseConnection.connectionUrl);
             Statement stmt = connection.createStatement();
+            //command = "INSERT INTO Episode(id, name, runtime, number,)";
             stmt.executeQuery(command);
         } catch (Exception e) {
             e.printStackTrace();
