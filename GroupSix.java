6535f08d1b7b77b2fd4f85f5c37dfaf6770d3477
Solid Wires <solid.wires@gmail.com>
2022-12-05 13:54:52 -0500

    Moved my statements into a method instead (just copy and paste my method and make your own CRUD statements)

--- GroupSix.java
+++ GroupSix.java
@@ -26,38 +26,54 @@ public class GroupSix {
         info.put("encrypt", true);
         info.put("trustServerCertificate", false);
         info.put("loginTimeout", 30);
+        
 
-        String command = "";
+        
         String output = "";
         try { 
             //TODO: Also, probably not safe due to SQL injection problem, so I'm going to try to see what I can do to fix that.
+            //Establish the connection
             Connection connection = DriverManager.getConnection(connUrl, info);
             Statement stmt = connection.createStatement();
-            //Creation
-            command = "INSERT INTO Episode(id, seasonID, episodeNumber, episodeName, description, runtime) VALUES (NULL, 1, 1, \"The Office Field Guide\", \"When a documentary crew arrives at the office, manager Michael Scott attempts to paint a rosy picture but fails after learning the company will be downsizing.\" 23);";
-            stmt.executeUpdate(command);
-            //Retrieval (must display the results using the ResultingSet object)
-            command = "SELECT * FROM Episode;";
-            ResultSet retrievalSet = stmt.executeQuery(command);
-            while(retrievalSet.next()) {
-                output += "\n Object ID " + String.valueOf(retrievalSet.getInt("id")) + ": ";
-                output += "\n\t Season: " + String.valueOf(retrievalSet.getInt("seasonID"));
-                output += "\n\t Episode: " + String.valueOf(retrievalSet.getInt("episodeNumber"));
-                output += "\n\t Name: " + retrievalSet.getString("episodeName");
-                output += "\n\t Description: " + retrievalSet.getString("description");
-                output += "\n\t Runtime: " + String.valueOf(retrievalSet.getInt("runtime"));
-            }
-            System.out.println("Selected Episode tuples: " + output);
-            output = "";
-            retrievalSet.close();
-            //Update
-            command = "UPDATE Episode SET episodeName=\"The Office Pilot Ep. 1: The Office Field Guide\" WHERE seasonID=1 AND episodeNumber=1;";
-            stmt.executeUpdate(command);
-            //Deletion
-            command = "DELETE FROM Episode WHERE seasonID=1 AND episodeNumber=1;";
-            stmt.executeUpdate(command);
+
+            //TvShow (KJ)
+            
+            //Season (Gavin)
+
+            //Episode (Jared)
+
+            //Actors in Movie? (Robert)
+            EpisodeQuery(stmt);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void EpisodeQuery(Statement stmt) throws Exception {
+        String command = "";
+        String output = "";
+
+        //Creation
+        command = "INSERT INTO Episode(id, seasonID, episodeNumber, episodeName, description, runtime) VALUES (NULL, 1, 1, \"The Office Field Guide\", \"When a documentary crew arrives at the office, manager Michael Scott attempts to paint a rosy picture but fails after learning the company will be downsizing.\" 23);";
+        stmt.executeUpdate(command);
+        //Retrieval (must display the results using the ResultingSet object)
+        command = "SELECT * FROM Episode;";
+        ResultSet retrievalSet = stmt.executeQuery(command);
+        while(retrievalSet.next()) {
+            output += "\n Object ID " + String.valueOf(retrievalSet.getInt("id")) + ": ";
+            output += "\n\t Season: " + String.valueOf(retrievalSet.getInt("seasonID"));
+            output += "\n\t Episode: " + String.valueOf(retrievalSet.getInt("episodeNumber"));
+            output += "\n\t Name: " + retrievalSet.getString("episodeName");
+            output += "\n\t Description: " + retrievalSet.getString("description");
+            output += "\n\t Runtime: " + String.valueOf(retrievalSet.getInt("runtime"));
+        }
+        System.out.println("Selected Episode tuples: " + output);
+        retrievalSet.close();
+        //Update
+        command = "UPDATE Episode SET episodeName=\"The Office Pilot Ep. 1: The Office Field Guide\" WHERE seasonID=1 AND episodeNumber=1;";
+        stmt.executeUpdate(command);
+        //Deletion
+        command = "DELETE FROM Episode WHERE seasonID=1 AND episodeNumber=1;";
+        stmt.executeUpdate(command);
     }
 }
