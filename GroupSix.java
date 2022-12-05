4096f7271e89d20c65cc8a184914c347a72512ff
Anderson <andersong6@winthrop.edu>
2022-12-05 14:19:27 -0500

    creation of gavin's crud statments in java

--- GroupSix.java
+++ GroupSix.java
@@ -35,40 +35,72 @@ public class GroupSix {
 
             //Replace the comments with your own methods
             //TvShow query (KJ)
+
             //Season query (Gavin)
-            EpisodeQuery(stmt);
+            private static void SeasonQuery(PreparedStatement stmt) throws Exception {
+                String command = "";
+                String output = "";
+        
+                //Creation
+                command = "INSERT INTO Season (id, seasonName, seasonNumber, tvShowID) VALUES (NULL, \"The Beginning\", 1, 1);";
+                stmt.executeUpdate(command);
+                //Retrieval (must display the results using the ResultingSet object)
+                command = "SELECT * FROM Season;";
+                ResultSet retrievalSet = stmt.executeQuery(command);
+                while(retrievalSet.next()) {
+                    output += "\n Object ID " + String.valueOf(retrievalSet.getInt("id")) + ": ";
+                    output += "\n\t Season: " + String.valueOf(retrievalSet.getInt("seasonID"));
+                    output += "\n\t Episode: " + String.valueOf(retrievalSet.getInt("episodeNumber"));
+                    output += "\n\t Name: " + retrievalSet.getString("episodeName");
+                    output += "\n\t Description: " + retrievalSet.getString("description");
+                    output += "\n\t Runtime: " + String.valueOf(retrievalSet.getInt("runtime"));
+                }
+                System.out.println("Selected Season tuples: " + output);
+                retrievalSet.close();
+                //Update
+                command = "UPDATE Season SET seasonName=\"True Beginning\" WHERE TVShowID=1 AND seasonNumber=1;";
+                stmt.executeUpdate(command);
+                //Deletion
+                command = "DELETE FROM Season WHERE tvShowID=1 AND seasonNumber=1;";
+                stmt.executeUpdate(command);
+            }
+
+            //Episode query (Jared)
+            private static void EpisodeQuery(PreparedStatement stmt) throws Exception {
+                String command = "";
+                String output = "";
+
+                //Creation
+                command = "INSERT INTO Episode(id, seasonID, episodeNumber, episodeName, description, runtime) VALUES (NULL, 1, 1, \"The Office Field Guide\", \"When a documentary crew arrives at the office, manager Michael Scott attempts to paint a rosy picture but fails after learning the company will be downsizing.\" 23);";
+                stmt.executeUpdate(command);
+                //Retrieval (must display the results using the ResultingSet object)
+                command = "SELECT * FROM Episode;";
+                ResultSet retrievalSet = stmt.executeQuery(command);
+                while(retrievalSet.next()) {
+                    output += "\n Object ID " + String.valueOf(retrievalSet.getInt("id")) + ": ";
+                    output += "\n\t Season: " + String.valueOf(retrievalSet.getInt("seasonID"));
+                    output += "\n\t Episode: " + String.valueOf(retrievalSet.getInt("episodeNumber"));
+                    output += "\n\t Name: " + retrievalSet.getString("episodeName");
+                    output += "\n\t Description: " + retrievalSet.getString("description");
+                    output += "\n\t Runtime: " + String.valueOf(retrievalSet.getInt("runtime"));
+                }
+                System.out.println("Selected Episode tuples: " + output);
+                retrievalSet.close();
+                //Update
+                command = "UPDATE Episode SET episodeName=\"The Office Pilot Ep. 1: The Office Field Guide\" WHERE seasonID=1 AND episodeNumber=1;";
+                stmt.executeUpdate(command);
+                //Deletion
+                command = "DELETE FROM Episode WHERE seasonID=1 AND episodeNumber=1;";
+                stmt.executeUpdate(command);
+            }
+
             //Actors in Movie (Robert)
+
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    //Episode query (Jared)
-    private static void EpisodeQuery(Statement stmt) throws Exception {
-        String command = "";
-        String output = "";
-
-        //Creation
-        command = "INSERT INTO Episode(id, seasonID, episodeNumber, episodeName, description, runtime) VALUES (NULL, 1, 1, \"The Office Field Guide\", \"When a documentary crew arrives at the office, manager Michael Scott attempts to paint a rosy picture but fails after learning the company will be downsizing.\" 23);";
-        stmt.executeUpdate(command);
-        //Retrieval (must display the results using the ResultingSet object)
-        command = "SELECT * FROM Episode;";
-        ResultSet retrievalSet = stmt.executeQuery(command);
-        while(retrievalSet.next()) {
-            output += "\n Object ID " + String.valueOf(retrievalSet.getInt("id")) + ": ";
-            output += "\n\t Season: " + String.valueOf(retrievalSet.getInt("seasonID"));
-            output += "\n\t Episode: " + String.valueOf(retrievalSet.getInt("episodeNumber"));
-            output += "\n\t Name: " + retrievalSet.getString("episodeName");
-            output += "\n\t Description: " + retrievalSet.getString("description");
-            output += "\n\t Runtime: " + String.valueOf(retrievalSet.getInt("runtime"));
-        }
-        System.out.println("Selected Episode tuples: " + output);
-        retrievalSet.close();
-        //Update
-        command = "UPDATE Episode SET episodeName=\"The Office Pilot Ep. 1: The Office Field Guide\" WHERE seasonID=1 AND episodeNumber=1;";
-        stmt.executeUpdate(command);
-        //Deletion
-        command = "DELETE FROM Episode WHERE seasonID=1 AND episodeNumber=1;";
-        stmt.executeUpdate(command);
-    }
 }
