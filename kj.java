29fe7595b19456db00b7334492b50d07404dcc51
andersong6 <andersong6@mailbox.winthrop.edu>
2022-12-04 23:25:36 -0500

    Change of kj's imports to include all sql libraries

--- kj.java
+++ kj.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLDatabaseConnection {
     public static String connectionUrl =
