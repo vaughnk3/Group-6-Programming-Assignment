ee47c7a56e473e5fec742a644e9d9faaf7e0c603
Solid Wires <solid.wires@gmail.com>
2022-12-04 22:39:48 -0500

    Placed the CRUD statements into my branch of code in such a way that I think would work... Will just need some testing.

--- Jared.java
+++ Jared.java
@@ -3,16 +3,38 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Jared {
 
     public static void query() {
         String command = "";
+        String output = "";
         try { 
             Connection connection = DriverManager.getConnection(SQLDatabaseConnection.connectionUrl);
             Statement stmt = connection.createStatement();
-            //command = "INSERT INTO Episode(id, name, runtime, number,)";
+            //Creation
+            command = "INSERT INTO Episode(id, seasonID, episodeNumber, episodeName, description, runtime) VALUES (NULL, 1, 1, \"The Office Field Guide\", \"When a documentary crew arrives at the office, manager Michael Scott attempts to paint a rosy picture but fails after learning the company will be downsizing.\" 23);";
+            stmt.executeQuery(command);
+            //Retrieval (must display the results using the ResultingSet object)
+            command = "SELECT * FROM Episode";
+            ResultSet retrievalSet = stmt.executeQuery(command);
+            while(retrievalSet.next()) {
+                output += "\n Object ID " + String.valueOf(retrievalSet.getInt("id")) + ": ";
+                output += "\n\t Season: " + String.valueOf(retrievalSet.getInt("seasonID"));
+                output += "\n\t Episode: " + String.valueOf(retrievalSet.getInt("episodeNumber"));
+                output += "\n\t Name: " + retrievalSet.getString("episodeName");
+                output += "\n\t Description: " + retrievalSet.getString("description");
+                output += "\n\t Runtime: " + String.valueOf(retrievalSet.getInt("runtime"));
+            }
+            System.out.println("Selected Episode tuples: " + output);
+            output = "";
+            //Update
+            command = "UPDATE Episode SET episodeName=\"The Office Pilot Ep. 1: The Office Field Guide\" WHERE seasonID=1 AND episodeNumber=1;";
+            stmt.executeQuery(command);
+            //Deletion
+            command = "DELETE FROM Episode WHERE seasonID=1 AND episodeNumber=1;";
             stmt.executeQuery(command);
         } catch (Exception e) {
             e.printStackTrace();
